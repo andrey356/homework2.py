5c2e2f078c6f4e3b75a613a5c480dc4787ca10a1
andrey356 <and.maschjanov@yandex.ru>
2024-06-10 09:42:50 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+number_of_homework_completed='12'
+print(number_of_homework_completed)
+number_of_hours_spent='1.5'
+print(number_of_hours_spent)
+course='Python'
+time_per_task=1.5/12
+print(time_per_task)
